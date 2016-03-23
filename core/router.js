9209ba058e76882ced66c68de9a4945958a02003
mucahitinel <mucahit.inel@hotmail.com>
2016-03-23 14:20:33 +0200

    basic routes added

---
+++ core/router.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+	fs = require("fs");
+
+module.exports = function (app) {
+	var controllers_path = process.cwd() + '/controller';
+
+	fs.readdirSync(controllers_path).forEach(function (file) {
+		if (file.indexOf('.js') != -1) {
+			controllers[file.split('.')[0]] = require(controllers_path + '/' + file);
+		}
+	});
+
+	var router = express.Router();
+
+	router.route("/users")
+		.post(controllers.user.save)
+		.get(controllers.user.list);
+
+	router.route("/users/:id")
+		.get(controllers.user.get)
+		.put(controllers.user.update)
+		.delete(controllers.user.delete);
+
+	router.route("/login")
+		.post(controllers.auth.login);
+
+	router.route("/register")
+		.post(controllers.auth.register);
+
+	return router;
+}
